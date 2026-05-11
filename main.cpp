d279e0ac9585c8af644845d56fbe5e17c6e11c33
Ruslana <shyshkoruslana08@gmail.com>
2026-05-11 21:03:48 +0300

    add counter and thread_id

--- main.cpp
+++ main.cpp
@@ -2,14 +2,27 @@
 #include <thread>
 
 using namespace std;
+int counter=0;
+void work(int thread_id){
+    for (int i = 0; i < 5; ++i) {
+        counter += thread_id;
+        cout<< "Thread: "<< thread_id << " Counter: " << counter << endl;
+    }
+}
 int main(){
 
     thread t1([](int x){cout << "Square: " << x * x << endl;},2);
     thread t2([](int x){cout << "Square: " << x * x << endl;},3);
     thread t3([](int x){cout << "Square: " << x * x << endl;},4);
-
     t1.join();
     t2.join();
     t3.join();
+
+    thread t4(work,1);
+    thread t5(work,2);
+    t4.join();
+    t5.join();
+    cout << "Final counter: " << counter << endl;
+
     return 0;
 }
